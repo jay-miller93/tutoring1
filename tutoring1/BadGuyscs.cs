c3301a479c27fd1790ccfc6f369cbfd82ad9db6a
Jay <isaacsdaddy42293@gmail.com>
2017-07-11 17:09:20 -0400

    tutoring

---
+++ tutoring1/BadGuyscs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutoring1
+{
+    class BadGuyscs
+    {
+        private string costumes;
+        private string name;
+        private string evilPowers;
+        private int strengthLevel;
+
+        public string Costumes { get; set; }
+        public string Name { get; set; }
+        public string EvilPowers { get; set; }
+        public int StrengthLevel { get; set; }
+
+        public BadGuyscs()
+        {
+            Random random = new Random();
+            this.costumes = "A bed sheet";
+            this.name = "Volcanotor";
+            this.evilPowers = "I can control volcanoes!!";
+            this.strengthLevel = random.Next(0, 51);
+        }
+
+        public BadGuyscs(string name, string costumes, int strengthLevel)
+        {
+            this.name = name;
+            this.costumes = costumes;
+            this.evilPowers = GetPowers();
+            this.strengthLevel = strengthLevel;
+        }
+        public string GetPowers()
+        {
+            string[] powers = { "Killer Farts", "Mind Control", "Throwing Fire", "Throwing Shade" };
+            Random random = new Random();
+            string newPower =powers[random.Next(0, powers.Length)];
+            return newPower;
+        }
+
+
+    }
+}
---
+++ tutoring1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tutoring1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BadGuyscs volcanoeMan = new BadGuyscs();
+            BadGuyscs badguy2 = new BadGuyscs("Ehhhh", "Street Clothes", 75);
+            Console.WriteLine(badguy2.GetPowers());
+        }
+    }
+}
